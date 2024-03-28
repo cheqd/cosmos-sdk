70a3ce5d886227547b2b1dd0bd2be55eecddbbd6
Tasos Derisiotis <50984242+Eengineer1@users.noreply.github.com>
2024-03-28 11:38:50 +0200

    fix: Remove height version mismatch guard

--- store/rootmulti/store.go
+++ store/rootmulti/store.go
@@ -243,9 +243,9 @@ func (rs *Store) loadVersion(ver int64, upgrades *types.StoreUpgrades) error {
 		// If it has been added, set the initial version
 		if upgrades.IsAdded(key.Name()) || upgrades.RenamedFrom(key.Name()) != "" {
 			storeParams.initialVersion = uint64(ver) + 1
-		} else if commitID.Version != ver && storeParams.typ == types.StoreTypeIAVL {
+		}/*  else if commitID.Version != ver && storeParams.typ == types.StoreTypeIAVL {
 			return fmt.Errorf("version of store %s mismatch root store's version; expected %d got %d; new stores should be added using StoreUpgrades", key.Name(), ver, commitID.Version)
-		}
+		} */
 
 		store, err := rs.loadCommitStoreFromParams(key, commitID, storeParams)
 		if err != nil {
